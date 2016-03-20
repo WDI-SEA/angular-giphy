94e412193d8d49cfcfb3c84c9cae254518d7a719
aheitzen <alexaheitzenrader@yahoo.com>
2016-03-20 11:34:47 -0700

    added basic functionality

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
-angular.module('GiphyApp', [])
+var app = angular.module('GiphyApp', ['infinite-scroll']);
 
-.run(function() {
-  console.log('App has loaded!');
-});
+function SearchCtrl($scope, $http) {
+	$scope.searchTerm = '';
+
+	$scope.getGiphies = function() {
+		if ($scope.searchTerm) {
+			$http.get('http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=' + $scope.searchTerm).then(function(response) {
+				$scope.giphies = response.data.data
+				// console.log($scope.giphies[0])
+			});
+		}
+	}
+}
+app.controller("SearchCtrl", SearchCtrl) 
