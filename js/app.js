9679129db8760bc76b68669a98b33a32ff7808af
navyayvan <navyayvan@gmail.com>
2016-03-20 22:45:41 -0700

    basic app done

--- js/app.js
+++ js/app.js
@@ -1,5 +1,32 @@
 angular.module('GiphyApp', [])
 
-.run(function() {
-  console.log('App has loaded!');
-});
+
+app.controller('SearchCtrl', ["$scope", "$http", function($scope, $http) {
+	$scope.searchTerm = '';
+	$scope.giphyResult = [];
+
+
+	$scope.$watch('searchTerm', function(newValue, oldValue) {
+		console.log('new value': newValue);
+		console.log('old value': oldValue);
+		console.log('...................');
+		$scope.search();
+	})
+
+// "http://api.giphy.com/v1/gifs/search?q=seahawks&api_key=dc6zaTOxFJmzC"
+	$scope.search = function() {
+		var req = {
+			url: "http://api.giphy.com/v1/gifs/search?q="+$scope.searchTerm+"&limit=100&api_keydc6zaTOxFJmzC"
+			method: 'GET',
+
+		}
+
+		$http(req).then(function success(res) {
+			console.log(res);
+		}, function error(res) {
+			console.log(res);
+		});
+	};
+}])
+
+
