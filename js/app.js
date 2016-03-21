4ca21e127a3d4a0cebc2fad5184aa0ca4e39f066
JonathanAndrew <jonandrewstevenson@yahoo.com>
2016-03-21 08:35:02 -0700

    giphy http reqeust along with css

--- js/app.js
+++ js/app.js
@@ -1,5 +1,33 @@
-angular.module('GiphyApp', [])
+var app = angular.module('GiphyApp', [])
 
-.run(function() {
-  console.log('App has loaded!');
-});
+console.log("hello");
+
+app.controller("GiphyCtrl",["$scope","$http", function($scope, $http){
+
+	$scope.searchTerm = '';
+	$scope.api = "dc6zaTOxFJmzC";
+	$scope.giphy = [];
+
+	$scope.search = function(){
+		var req = {
+			url: 'http://api.giphy.com/v1/gifs/search',
+			method: 'GET',
+			params: {
+				api_key : $scope.api,
+				q : $scope.searchTerm, 
+			} 
+		
+	};
+
+	$http(req).then(function success(res){
+		$scope.giphy = res.data.data;
+		console.log($scope.giphy);
+
+	}, function error(res){
+			console.log(res);
+		});
+	
+	}
+
+
+}]);
