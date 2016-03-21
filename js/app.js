c10d54edadd751bdeb5ae3fc9e0132453b667ce3
audreyandoy <audreylandoy@gmail.com>
2016-03-21 09:44:18 -0700

    finished homework

--- js/app.js
+++ js/app.js
@@ -1,5 +1,41 @@
-angular.module('GiphyApp', [])
+var app = angular.module("GiphyApp", []);
 
-.run(function() {
+app.controller('GiphyCtrl', ['$scope', '$http', function($scope, $http) {
   console.log('App has loaded!');
-});
+
+  $scope.searchTerm = '';
+  $scope.gifs = [];
+
+  $scope.$watch('searchTerm', function(){
+  //     console.log('new value:', newValue);
+  //     console.log('old value:', oldValue);
+  //     console.log('-------------------------');
+      $scope.search();
+
+  });
+
+   $scope.search = function() {
+    // console.log($scope.searchTerm);
+    var req = {
+      url: 'http://api.giphy.com/v1/gifs/search',    
+      method: 'GET',
+      params: {
+        q: $scope.searchTerm,
+        api_key: "dc6zaTOxFJmzC"
+      }
+    };
+
+    $http(req).then(function success(res) {
+        $scope.gifs = res.data.data;
+        console.log($scope.gifs);
+    }, function error(res) {
+        console.log(res);
+    });
+
+  }
+
+
+
+  
+}]);
+
