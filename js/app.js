ecd4ee463b5f534cf4c2a2a4c54bd826b3ef52e7
Josh McBroom <mcbroomjc@gmail.com>
2016-07-23 14:00:51 -0700

    Done.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,21 @@
-angular.module('GiphyApp', [])
+var app = angular.module('GiphyApp', []);
 
-.run(function() {
-  console.log('App has loaded!');
-});
+app.controller('SearchCtrl', ['$scope', '$http', function($scope, $http) {
+
+  $scope.userSearch = '';
+
+  $scope.searchForGifs = function() {
+    $http.get('http://api.giphy.com/v1/gifs/search?', {
+      params: {
+        q: $scope.userSearch,
+        api_key: 'dc6zaTOxFJmzC'
+      }
+    })
+    .then(function success(res) {
+      $scope.giphyResults = res.data.data;
+      console.log($scope.giphyResults);
+    }, function error(res) {
+      console.log(res);
+    });
+  };
+}]);
