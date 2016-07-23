d0687c92e448ebf49ab269f1055de367e82d6c48
Ivar Lee <ivarr5@gmail.com>
2016-07-23 16:39:06 -0700

    working on infinite scroll

--- js/app.js
+++ js/app.js
@@ -1,5 +1,46 @@
-angular.module('GiphyApp', [])
+var app = angular.module('GiphyApp', ['infinite-scroll']);
 
-.run(function() {
-  console.log('App has loaded!');
-});
+app.controller('GiphySearch', ['$scope', '$http', function($scope,$http) {
+
+  $scope.userSearch = '';
+
+  $scope.$watch('userSearch', function(newVal, oldVal) {
+    console.log($scope.userSearch)
+    $http.get('http://api.giphy.com/v1/gifs/search?', {
+      params: {
+        q: $scope.userSearch,
+        api_key: 'dc6zaTOxFJmzC',
+        limit: 6
+      }
+    })
+    .then(function success(response) {
+      console.log(response.data.data);
+      $scope.giphyResults = response.data.data;
+
+    }, function error(response) {
+      console.log(response);
+    });
+  });
+
+  $scope.page = 0;
+
+  $scope.searchForMoreGifs = function() {
+    $scope.page += 1;
+    $http.get('http://api.giphy.com/v1/gifs/search?', {
+      params: {
+        q: $scope.userSearch,
+        api_key: 'dc6zaTOxFJmzC',
+        limit: 6,
+        offset: $scope.page
+      }
+    })
+    .then(function success(response) {
+      console.log(response.data.data);
+      $scope.giphyResults = response.data.data;
+
+    }, function error(response) {
+      console.log(response);
+    });
+  };
+
+}]);
