9272321e3f19d3a429d8a3f51a8fbb159428f249
Krista Calderon <kamsci@gmail.com>
2016-07-24 20:23:27 -0700

    can search for topic and see gifs

--- js/app.js
+++ js/app.js
@@ -1,5 +1,29 @@
-angular.module('GiphyApp', [])
+var app = angular.module('GiphyApp', [])
 
 .run(function() {
   console.log('App has loaded!');
 });
+
+app.controller('Ctrl', ['$scope', '$http', function($scope, $http) {
+  console.log("controller active")
+
+  $scope.searchTerm = null;
+
+  $scope.search = function() {
+    $http.get('http://api.giphy.com/v1/gifs/search', {
+      params: {
+        q: $scope.searchTerm,
+        api_key: 'dc6zaTOxFJmzC',
+        limit: 50
+      }
+    }).then(function success(response) {
+      console.log("success", $scope.searchTerm);
+      $scope.results = response.data.data;
+      $scope.searchTerm = null;
+      console.log($scope.results)
+    }, function error(response) {
+      console.log("Failed", $scope.searchTerm);
+    });
+  }
+
+}]) // end Ctrl'er
