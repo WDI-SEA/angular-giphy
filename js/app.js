8885d61af8125d832bdb1596f363a0911e651da5
Jacob-Elder <jacobelderonly@gmail.com>
2016-10-26 22:02:30 -0700

    giphy search functionality works

--- js/app.js
+++ js/app.js
@@ -1,5 +1,24 @@
-angular.module('GiphyApp', [])
+var app = angular.module('GiphyApp', [])
+var api_base_url = "http://api.giphy.com/v1/gifs/search?q=";
+var api_key = "&api_key=dc6zaTOxFJmzC";
 
-.run(function() {
-  console.log('App has loaded!');
-});
+app.controller("giphyCtrl", ["$scope", "$http", function($scope, $http){
+	console.log("controller loaded!");
+	console.log(api_key);
+
+	$scope.gifs = [];
+
+	$scope.getGif = function(searchTerm){
+		$http({
+			url: api_base_url + searchTerm + api_key,
+			method: "GET",
+			params: {
+				limit: 25
+			}
+		}).then(function(response){
+			$scope.gifs = response.data.data;
+			console.log($scope.gifs);
+		});
+	};
+
+}]);
