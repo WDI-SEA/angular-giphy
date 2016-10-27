9547019edd75686dd083c606c77e76a38e990ce8
sarahpappas <pappas.sarah@gmail.com>
2016-10-26 20:05:54 -0700

    works

--- js/app.js
+++ js/app.js
@@ -1,5 +1,21 @@
-angular.module('GiphyApp', [])
-
+var app = angular.module('GiphyApp', [])
 .run(function() {
   console.log('App has loaded!');
 });
+
+app.controller("HomeCtrl", ["$scope", "GiphyService", function($scope, GiphyService) {
+	$scope.gifs = [];
+	$scope.searchTerm = "";
+	
+	$scope.search = function() {
+		if($scope.searchTerm) {
+			GiphyService.getGifs($scope.searchTerm).then(function(response) {
+				$scope.gifs = response.data.data;
+				console.log($scope.gifs);
+			});
+		}
+		$scope.searchTerm = "";
+	}
+
+
+}]);
