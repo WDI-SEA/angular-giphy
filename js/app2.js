02f1be3b3aabee01701bed672193e193283409ea
kimmabrandt <kimmabrandt@gmail.com>
2016-10-27 09:07:16 -0700

    fixed some indentation

--- js/app2.js
+++ js/app2.js
@@ -7,50 +7,36 @@ app.controller("GiphyController", ['$scope', '$http', function($scope, $http){
 	$scope.error = "";
 	$scope.search = "";
 
-
-$scope.$watch("searchTerm", function(newVal, oldVal){
-    if($scope.searchOnType){
-      $scope.search(newVal);
-    }
-  });
-
-
-$scope.search = function() {
-	if(!$scope.searchTerm){
-      $scope.error = "You need to provide something for me to search";
-      return;
-    }
-    else if(!isAlphaNumeric($scope.searchTerm)){
-      $scope.error = "Invalid characters detected.";
-      return;
-    }
-    //2. Set up the request to OMDb
-    $http({
-      url: "http://api.giphy.com/v1/gifs/search?q=" + $scope.searchTerm + "&api_key=dc6zaTOxFJmzC"
-      // api_key: "dc6zaTOxFJmzC",
-      // method: "GET",
-      // params: {
-      //   q: $scope.searchTerm
-      //   //specific to omdb api (like q on reddit)
-      // }
-    }).then(function success(results){
-      //success: it worked
-      $scope.error = "";
-      $scope.gifs = results.data.data;
-      console.log(results.data.data);
-    }, function error(res){
-      //error: uh oh, didn't work
-      $scope.error = "The request failed. See console for details.";
-      console.log(res);
+  $scope.$watch("searchTerm", function(newVal, oldVal){
+      if($scope.searchOnType){
+        $scope.search(newVal);
+      }
     });
 
-}
-
-
-
+  $scope.search = function() {
+  	if(!$scope.searchTerm){
+        $scope.error = "You need to provide something for me to search";
+        return;
+      }
+      else if(!isAlphaNumeric($scope.searchTerm)){
+        $scope.error = "Invalid characters detected.";
+        return;
+      }
+      //2. Set up the request
+      $http({
+        url: "http://api.giphy.com/v1/gifs/search?q=" + $scope.searchTerm + "&api_key=dc6zaTOxFJmzC"
+      }).then(function success(results){
+        //success: it worked
+        $scope.error = "";
+        $scope.gifs = results.data.data;
+        console.log(results.data.data);
+      }, function error(res){
+        //error: uh oh, didn't work
+        $scope.error = "The request failed. See console for details.";
+        console.log(res);
+      });
+  }
 }]); //end controller
-
-
 
 function isAlphaNumeric(input){
   return /^[a-z\d\-_\s]+$/i.test(input);
