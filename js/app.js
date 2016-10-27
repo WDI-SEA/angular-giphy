0133536192cce57f6de602070c28be66c7f021f2
tannerkovach <kovach.tanner@gmail.com>
2016-10-26 18:39:12 -0700

    everything works-infinite scroll works, but doesnt run smoothly

--- js/app.js
+++ js/app.js
@@ -1,5 +1,41 @@
-angular.module('GiphyApp', [])
 
-.run(function() {
-  console.log('App has loaded!');
-});
+var app = angular.module('GiphyApp', ['infinite-scroll'])
+app.controller("GiphyCtrl", ['$scope', "$http", function($scope, $http) {
+	  
+	  $scope.searchTerm = "";
+	  $scope.gifs = [];
+	  $scope.error = "";
+	  $scope.count = 0;
+	  $scope.offset = 0;
+	  $scope.oldSearchTerm = "";
+
+	  // $scope.searchOnType = false;
+
+	  console.log();
+
+	 $scope.search = function() {
+    $http.get('http://api.giphy.com/v1/gifs/search?', {
+      params: {
+        q: $scope.searchTerm,
+        offset: $scope.offset,
+        api_key: 'dc6zaTOxFJmzC',
+        limit: 15
+      }
+    }).then(function success(res) {
+      $scope.gifs = res.data.data;
+    }, function error(res) {
+      console.log(res);
+    });
+
+    }
+
+
+      $scope.myPagingFunction = function (){
+        $scope.oldSearchTerm = $scope.searchTerm;
+        $scope.offset = 25 * $scope.count;
+        $scope.search();
+        $scope.count++;
+    };
+}]);
+
+
