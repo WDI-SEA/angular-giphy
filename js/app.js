0373f6bba13ffba0f690bdade6d91f97684d182f
jamiebb <jamieandburton@gmail.com>
2016-10-26 21:30:15 -0700

    Deliverable Completed

--- js/app.js
+++ js/app.js
@@ -1,5 +1,24 @@
-angular.module('GiphyApp', [])
-
-.run(function() {
+var app = angular.module('GiphyApp', []).run(function() {
   console.log('App has loaded!');
 });
+
+app.controller("GiphyCtrl", ["$scope", "$http", function($scope, $http){
+
+$scope.giphys = [];
+$scope.searchTerm = ""
+
+$scope.search = function(){
+
+var url = "http://api.giphy.com/v1/gifs/search?q=" + $scope.searchTerm + "&api_key=dc6zaTOxFJmzC";
+
+	$http({
+		url: url,
+		method: 'GET',
+		params: {}
+	}).then (function success(results){
+		$scope.giphys = results.data.data;
+	})
+	
+};
+
+}])
