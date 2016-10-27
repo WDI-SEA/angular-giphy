560e84b6f68a7b80ffb51ce5f32b8027cfc3b16f
hayes9321 <t.hayes8321@gmail.com>
2016-10-27 09:07:26 -0700

    complete

--- js/app.js
+++ js/app.js
@@ -1,5 +1,19 @@
-angular.module('GiphyApp', [])
-
+var app = angular.module('GiphyApp', [])
 .run(function() {
   console.log('App has loaded!');
 });
+
+app.controller("GiphyCtrl", ["$scope", "ServiceController", function($scope, ServiceController) {
+	$scope.giphy = [];
+	$scope.searchTerm = "";
+	
+	$scope.searchFor = function() {
+		if($scope.searchTerm) {
+			ServiceController.getGifs($scope.searchTerm).then(function(response) {
+				$scope.gifs = response.data.data;
+				console.log($scope.gifs);
+			});
+		}
+		$scope.searchTerm = "";
+	}
+}]);
