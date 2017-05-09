e47d7668dc9898dde0bbfe44ee44befcd095f52c
celgutierrez <celgutierrez42@gmail.com>
2017-05-09 16:39:42 -0700

    finished lab

--- js/app.js
+++ js/app.js
@@ -1,5 +1,23 @@
 angular.module('GiphyApp', [])
 
-.run(function() {
-  console.log('App has loaded!');
-});
+
+
+.controller('GiphySearch', ['$scope', '$http', function($scope, $http) {
+    $scope.giphyList = [];
+
+    $scope.search = function() {
+        var req = {
+            url: 'http://api.giphy.com/v1/gifs/search',
+            method: 'GET',
+            params: {
+                q: $scope.searchGif,
+                api_key: 'dc6zaTOxFJmzC'
+            }
+        }
+        $http(req).then(function(response) {
+            $scope.giphyList = response.data.data;
+        })
+    }
+
+
+}]);
