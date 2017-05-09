07b0dab815ee6e49d3bfb9a60d677fb7dd418ff8
alainabuzas <alaina.buzas@gmail.com>
2017-05-09 15:52:32 -0700

    working on lab

--- js/app.js
+++ js/app.js
@@ -1,5 +1,24 @@
-angular.module('GiphyApp', [])
+var giphyApp = angular.module('GiphyApp', []);
 
-.run(function() {
-  console.log('App has loaded!');
-});
+giphyApp.controller('GiphySearch', ['$scope', '$http', function($scope, $http) {
+    $scope.giphyList = [];
+
+    $scope.search = function() {
+        console.log('sent a search')
+        var req = {
+            url: 'http://api.giphy.com/v1/gifs/search',
+            method: 'GET',
+            params: {
+                q: $scope.searchTerm,
+                api_key: 'dc6zaTOxFJmzC'
+            }
+        }
+
+        $http(req).then(function(response) {
+            console.log('came back!')
+            console.log(response)
+            $scope.giphyList = response.data.Search;
+        });
+    }
+
+}]);
