a8e6700f4f289df066563727a0fdf5b3e04c24e1
alainabuzas <alaina.buzas@gmail.com>
2017-05-09 17:00:42 -0700

    no errors but scroll isnt working

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
-var giphyApp = angular.module('GiphyApp', []);
+var giphyApp = angular.module('GiphyApp', ['tagged.directives.infiniteScroll']);
 
 giphyApp.controller('GiphySearch', ['$scope', '$http', function($scope, $http) {
+    $scope.page = 1;
     $scope.giphyList = [];
+    $scope.fetching = false;
 
     $scope.search = function() {
         console.log('sent a search')
@@ -13,6 +15,16 @@ giphyApp.controller('GiphySearch', ['$scope', '$http', function($scope, $http) {
                 api_key: 'dc6zaTOxFJmzC'
             }
         }
+
+        $scope.getMore = function() {
+            $scope.page++;
+            $scope.fetching = true;
+            $http.get('/my/endpoint', { page: $scope.page }).then(function(giphyList) {
+                $scope.fetching = false;
+                $scope.giphyList = $scope.giphyList.concat(giphyList);
+            });
+        };
+
 
         $http(req).then(function(response) {
             console.log('came back!')
