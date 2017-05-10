9b3784a259983c8669aef52b01ec3786e8c7f2cf
buffalocheck <shawn@buffalocheck.com>
2017-05-10 05:54:56 -0700

    tue night

--- js/app.js
+++ js/app.js
@@ -1,5 +1,26 @@
-angular.module('GiphyApp', [])
+var giphyApp = angular.module('GiphyApp', []);
 
-.run(function() {
-  console.log('App has loaded!');
-});
+giphyApp.controller('SearchCtrl', ['$scope', '$http', function($scope, $http) {
+    $scope.gifList = [];
+
+    $scope.search = function() {
+        var req = {
+            url: "http://api.giphy.com/v1/gifs/search",
+            method: "GET",
+            params: {
+                q: $scope.searchTerm,
+                api_key: "dc6zaTOxFJmzC"
+            }
+        };
+
+        $http(req).then(function(response) {
+            console.log(response.data.data);
+            $scope.gifList = response.data.data;
+        });
+    };
+
+    $scope.$watch("searchTerm", function(newVal, oldVal) {
+        $scope.search();
+    });
+
+}]);
